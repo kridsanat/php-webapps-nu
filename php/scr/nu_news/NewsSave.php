8c46c1b221046555a7d12acd6f7ed589fbc935d5
BLOOM <k-tanom@hotmail.com>
2024-05-13 12:07:20 +0700

    Update NewsSave.php

--- php/scr/nu_news/NewsSave.php
+++ php/scr/nu_news/NewsSave.php
@@ -3,50 +3,59 @@
 
 ob_start();
 $useradmin = $_SESSION["useradmin"];
-if(empty($useradmin)) 
-{
-echo "<script>alert('Only Administrator');</script>";
-header("Location: ../index.php");
-exit();
+if(empty($useradmin)) {
+    echo "<script>alert('Only Administrator');</script>";
+    header("Location: ../index.php");
+    exit();
 }
+
 require_once "../include/tdate.php";
 require_once "../include/connectdb.php";
 
-						  $sql="select * from useradmin where useradmin='$useradmin'";
-						  $db_query=mysqli_query($connect, $sql);					
-						  $result=mysqli_fetch_array($db_query);
-						  $id=$result["id"];
-						  $adminname=$result["name"];
-						  $user_admin=$result["useradmin"];
-						  $pass_admin=$result["passadmin"];
-						  
-if($_FILES["newphoto"]["name"] != "")
-{
-$name=$_FILES['newphoto']['name'];
-$tmp=$_FILES['newphoto']["tmp_name"];
-$date_time=date("Y-m-d H:i:s");
-$oldname=explode(".",$name);
-$ext = "";
-$ext = ".".$oldname[count($oldname)-1];
-$photo_1 =date('YmdHis').$ext;
-copy($tmp,"NEW/".$photo_1);
-}else{
-$photo_1 = "";
+$sql = "SELECT * FROM useradmin WHERE useradmin=?";
+$stmt = mysqli_prepare($connect, $sql);
+mysqli_stmt_bind_param($stmt, "s", $useradmin);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+
+if($result && $result->num_rows > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $id = $row["id"];
+    $adminname = $row["name"];
+    $user_admin = $row["useradmin"];
+    $pass_admin = $row["passadmin"];
+} else {
+    echo "<script>alert('Invalid User');</script>";
+    header("Location: ../index.php");
+    exit();
 }
-						  
-$postmessage = str_replace("\n","<br>",$_POST['message']);
 
+$photo_1 = "";
+if(isset($_FILES["newphoto"]["name"]) && $_FILES["newphoto"]["name"] != "") {
+    $name = $_FILES['newphoto']['name'];
+    $tmp = $_FILES['newphoto']['tmp_name'];
+    $date_time = date("Y-m-d H:i:s");
+    $oldname = explode(".", $name);
+    $ext = "." . end($oldname);
+    $photo_1 = date('YmdHis') . $ext;
+    move_uploaded_file($tmp, "NEW/" . $photo_1);
+}
 
+$postmessage = str_replace("\n", "<br>", $_POST['message']);
 
-$insert = "INSERT INTO news (topic, newphoto, message, dateregist) VALUES ('$_POST[topic]', '$photo_1', '$postmessage', '$e_date $etime')";
-$result = mysqli_query($connect, $insert);
-if (!$result) {
+$sql = "INSERT INTO news (topic, newphoto, message, dateregist) VALUES (?, ?, ?, ?)";
+$stmt = mysqli_prepare($connect, $sql);
+mysqli_stmt_bind_param($stmt, "ssss", $_POST['topic'], $photo_1, $postmessage, "$e_date $etime");
+$result = mysqli_stmt_execute($stmt);
+
+if(!$result) {
     die("Cannot Add Database: " . mysqli_error($connect));
 }
 ?>
+
 <html>
 <head>
-<title><? echo "$headtxt"; ?></title>
+<title><?php echo "$headtxt"; ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=tis-620">
 <meta http-equiv="refresh" content="900;url=../logout.php" />
 
@@ -56,30 +65,28 @@ if (!$result) {
 </head>
 <body bgcolor="#ffffff">
 <div align="center"> 
-
-            <table width="100%" border="0" cellspacing="1" cellpadding="1">
-                <tr> 
-                    <td>
-						<div align="center">
-						<br>
-                        <br>	
-		<img src="../images/ajax-loader.gif" width="" height="">			
-                        <br>
-                        <br>
-        <font color="#003366" size="2">Complated</font>
-						<br>
-						<br> 
-		<font size="2">Please Wait...</font>
-						<br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-						
-						</div>
-					</td>
-                </tr>
-            </table>
+    <table width="100%" border="0" cellspacing="1" cellpadding="1">
+        <tr> 
+            <td>
+                <div align="center">
+                <br>
+                <br>    
+                <img src="../images/ajax-loader.gif" width="" height="">           
+                <br>
+                <br>
+                <font color="#003366" size="2">Completed</font>
+                <br>
+                <br> 
+                <font size="2">Please Wait...</font>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                </div>
+            </td>
+        </tr>
+    </table>
 </div>
 </body>
 </html>
