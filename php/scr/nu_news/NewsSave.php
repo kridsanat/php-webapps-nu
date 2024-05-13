a377e303e134cf7fb014374deaba33fc838ca88a
BLOOM <k-tanom@hotmail.com>
2024-05-13 12:21:13 +0700

    Update NewsSave.php

--- php/scr/nu_news/NewsSave.php
+++ php/scr/nu_news/NewsSave.php
@@ -3,7 +3,7 @@
 
 ob_start();
 $useradmin = $_SESSION["useradmin"];
-if(empty($useradmin)) {
+if (empty($useradmin)) {
     echo "<script>alert('Only Administrator');</script>";
     header("Location: ../index.php");
     exit();
@@ -18,7 +18,7 @@ mysqli_stmt_bind_param($stmt, "s", $useradmin);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 
-if($result && $result->num_rows > 0) {
+if ($result && $result->num_rows > 0) {
     $row = mysqli_fetch_assoc($result);
     $id = $row["id"];
     $adminname = $row["name"];
@@ -31,7 +31,7 @@ if($result && $result->num_rows > 0) {
 }
 
 $photo_1 = "";
-if(isset($_FILES["newphoto"]["name"]) && $_FILES["newphoto"]["name"] != "") {
+if (isset($_FILES["newphoto"]["name"]) && $_FILES["newphoto"]["name"] != "") {
     $name = $_FILES['newphoto']['name'];
     $tmp = $_FILES['newphoto']['tmp_name'];
     $date_time = date("Y-m-d H:i:s");
@@ -55,42 +55,42 @@ $datetime = mysqli_real_escape_string($connect, "$e_date $etime");
 
 $result = mysqli_stmt_execute($stmt);
 
-if(!$result) {
+if (!$result) {
     die("Cannot Add Database: " . mysqli_error($connect));
 }
 ?>
 
 <html>
 <head>
-<title><?php echo htmlspecialchars($headtxt); ?></title>
-<meta http-equiv="Content-Type" content="text/html; charset=tis-620">
+    <title><?php echo htmlspecialchars($headtxt); ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=tis-620">
 
-<!-- Remove one of the meta refresh tags if not necessary -->
-<meta http-equiv="refresh" content="900;url=../logout.php">
+    <!-- Remove one of the meta refresh tags if not necessary -->
+    <meta http-equiv="refresh" content="900;url=../logout.php">
 
-<link href="css/style.css" rel="stylesheet" type="text/css">
-<meta http-equiv="refresh" content="3;URL=newmain.php">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <meta http-equiv="refresh" content="3;URL=newmain.php">
 </head>
 <body bgcolor="#ffffff">
-<div align="center"> 
+<div align="center">
     <table width="100%" border="0" cellspacing="1" cellpadding="1">
-        <tr> 
+        <tr>
             <td>
                 <div align="center">
-                <br>
-                <br>    
-                <img src="../images/ajax-loader.gif" width="" height="">           
-                <br>
-                <br>
-                <font color="#003366" size="2">Completed</font>
-                <br>
-                <br> 
-                <font size="2">Please Wait...</font>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
+                    <br>
+                    <br>
+                    <img src="../images/ajax-loader.gif" width="" height="">
+                    <br>
+                    <br>
+                    <font color="#003366" size="2">Completed</font>
+                    <br>
+                    <br>
+                    <font size="2">Please Wait...</font>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
                 </div>
             </td>
         </tr>
