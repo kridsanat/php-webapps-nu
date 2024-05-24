79538f1ed8168cf12e7ed5a916f7acb9d55c4e82
BLOOM <k-tanom@hotmail.com>
2024-05-24 10:11:16 +0700

    up

--- php/scr/nu_news/NewsSave-Copy.php
+++ php/scr/nu_news/NewsSave-Copy.php
@@ -1,16 +1,15 @@
 <?php
 // Start session and output buffering
-session_start();
+@session_start();
 ob_start();
 
 // Check if user is logged in as administrator
-if (empty($_SESSION["useradmin"])) {
+$useradmin = $_SESSION["useradmin"];
+if(empty($useradmin)) {
     echo "<script>alert('Only Administrator');</script>";
     header("Location: ../index.php");
     exit();
 }
-
-$useradmin = $_SESSION["useradmin"];
 
 // Include necessary files
 require_once "../include/tdate.php";
@@ -24,7 +23,7 @@ mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 
 // Check if user information is found
-if ($result && mysqli_num_rows($result) > 0) {
+if($result && $result->num_rows > 0) {
     $row = mysqli_fetch_assoc($result);
     // Retrieve necessary user information if needed
 } else {
@@ -35,24 +34,16 @@ if ($result && mysqli_num_rows($result) > 0) {
 
 // Process file upload if applicable
 $photo_1 = "";
-if (isset($_FILES["newphoto"]["name"]) && $_FILES["newphoto"]["name"] != "") {
-    $name = basename($_FILES['newphoto']['name']);
+if(isset($_FILES["newphoto"]["name"]) && $_FILES["newphoto"]["name"] != "") {
+    $name = $_FILES['newphoto']['name'];
     $tmp = $_FILES['newphoto']['tmp_name'];
     // Generate unique file name and move the file to the desired location
     $photo_1 = "NEW/" . uniqid() . '_' . $name;
-    if (!move_uploaded_file($tmp, $photo_1)) {
-        echo "<script>alert('File upload failed');</script>";
-        header("Location: ../index.php");
-        exit();
-    }
+    move_uploaded_file($tmp, $photo_1);
 }
 
 // Sanitize user input for message
-$postmessage = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');
-
-// Retrieve current date and time for the database record
-$e_date = date('Y-m-d');
-$etime = date('H:i:s');
+$postmessage = htmlspecialchars($_POST['message']);
 
 // Construct SQL INSERT statement
 $sql = "INSERT INTO news (topic, newphoto, message, dateregist) VALUES (?, ?, ?, ?)";
@@ -61,12 +52,8 @@ mysqli_stmt_bind_param($stmt, "ssss", $_POST['topic'], $photo_1, $postmessage, "
 $result = mysqli_stmt_execute($stmt);
 
 // Check if SQL INSERT was successful
-if (!$result) {
+if(!$result) {
     die("Cannot Add Database: " . mysqli_error($connect));
-} else {
-    echo "<script>alert('News added successfully');</script>";
-    header("Location: success_page.php"); // Redirect to a success page or another appropriate page
-    exit();
 }
 ?>
 
