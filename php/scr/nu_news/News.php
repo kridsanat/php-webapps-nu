9375e62e9b4f670af9d3bc787c7c13cbb6368768
BLOOM <k-tanom@hotmail.com>
2024-05-24 11:10:38 +0700

    up

--- php/scr/nu_news/News.php
+++ php/scr/nu_news/News.php
@@ -3,109 +3,132 @@
 
 ob_start();
 $useradmin = $_SESSION["useradmin"];
-if(empty($useradmin)) {
-    echo "<script>alert('Only Administrator');</script>";
-    header("Location: ../index.php");
-    exit();
+if(empty($useradmin)) 
+{
+echo "<script>alert('Only Administrator');</script>";
+header("Location: ../index.php");
+exit();
 }
 require_once "../include/tdate.php";
-// Removed unnecessary database query
+require_once "../include/connectdb.php";
+
+						  $sql="select * from useradmin where useradmin='$useradmin'";
+						  $db_query=mysqli_query($connect, $sql);					
+						  $result=mysqli_fetch_array($db_query);
+						  $id=$result["id"];
+						  $adminname=$result["name"];
+						  $user_admin=$result["useradmin"];
+						  $pass_admin=$result["passadmin"];
+
+
+
 ?>
 
 <html>
-<head>
-    <title><?php echo "$headtxt_web"; ?></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=tis-620">
-    <meta http-equiv="refresh" content="900;url=../logout.php" />
-    <!-- Add appropriate links to CSS and other resources -->
-</head>
+
+		<head>
+			<title><?php echo "$headtxt_web"; ?></title>
+			<meta http-equiv="Content-Type" content="text/html; charset=tis-620">
+      <meta http-equiv="refresh" content="900;url=../logout.php" />
+			<link href="" rel="" type="">
+		</head>
 <body bgcolor="#ffffff">
-<div align="center">
-    <font color="#990000" size="+1"><strong><font color="#333333" size="2">
-        <br><br>
-
-        <script language="JavaScript">
-            function checksearch() {
-                var news1 = document.webFormSearch.topic.value;
-                var news2 = document.webFormSearch.message.value;
-
-                if (news1.length == 0) {
-                    alert("Please enter a title");
-                    document.webFormSearch.topic.focus();
-                    return false;
-                } else if (news2.length == 0) {
-                    alert("Please enter a detail");
-                    document.webFormSearch.message.focus();
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        </script>
-
-        <table width="700" border="0" align="center" cellpadding="1" cellspacing="1">
-            <form method="post" action="NewsSave.php" enctype="multipart/form-data" name="webFormSearch"
-                  onSubmit="return checksearch()">
-                <tr bgcolor="#FFFFFF">
-                    <td width="85" valign="top">
-                        <div align="right"><font size="2">Title:</div>
-                    </td>
-                    <td><font size="3">
-                            <input name="topic" type="text" class="input" id="topic" size="120"
-                                   value="Nutrition Profess | ">
-                        </font></td>
-
-                </tr>
-                <tr>
-                    <td align="right">
-                        <font size="3"> Picture:</font>
-
-                    </td>
-                    <td>
-                        <input name="newphoto" type="file" class="input" id="newphoto" size="40">
-                    </td>
-                </tr>
-                <tr bgcolor="#FFFFFF">
-                    <td rowspan="2" valign="top">
-                        <div align="right"><font size="2">Detail:</font></div>
-                    </td>
-                    <td width="469" rowspan="2">
-                        <font size="3">
-                            <textarea name="message" cols="180" rows="30" class="input" id="message">
-                                <div align="left">
-                                    <font color="#000000" face="tahoma" size="3">
-                                        <a href="." target="_blank"></a>
-                                    </font>
-                                </div>
-                            </textarea>
-                        </font>
-                        <br><br>
-                        Update on :<input name="update" type="text" class="input" id="update" size="12"
-                                          value='<?php echo $e_date; ?>' maxlength="100" readonly> *Cannot edit !
-                    </td>
-                </tr>
-                <tr bgcolor="#FFFFFF">
-                    <td width="29" class="input">
-                        <p><br></p></td>
-                </tr>
-
-                <tr bgcolor="#FFFFFF">
-                    <td valign="top">&nbsp;</td>
-
-                    <td><font size="3">
-                            <br>
-                            <input name="submit" type="submit" class="submit" value="Submit"
-                                   onClick="return confirm ('Sure ? ') ">
-                            &nbsp;&nbsp;&nbsp;&nbsp;
-                            <a href="newmain.php" target="">Cancel</a>
-                        </font>
-                    </td>
-
-                </tr>
-            </form>
-        </table>
+<div align="center"><font color="#990000" size="+1"><strong><font color="#333333" size="2"> 
+  </font></strong></font>
 
 
+<br><br>
+
+ 
+<script language="JavaScript">
+function checksearch()
+{
+      var news1 = document.webFormSearch.topic.value;
+	  var news2 = document.webFormSearch.message.value;
+
+  if( news1.length ==0)
+           {
+           alert("��س������Ǣ�͢��Ǵ��¤��");
+           document.webFormSearch.topic.focus();           
+           return false;
+           }else if( news2.length ==0)
+           {
+           alert("��س������������´���Ǵ��¤��");
+           document.webFormSearch.message.focus();           
+           return false;
+           }
+         else
+           return true;
+}
+</script>
+                                <table width="700" border="0" align="center" cellpadding="1" cellspacing="1">
+                                  <form method="post" action="NewsSave.php" enctype="multipart/form-data" name="webFormSearch" onSubmit="return checksearch()">
+                                    <tr bgcolor="#FFFFFF"> 
+                                      <td width="85" valign="top"> <div align="right"><font size="2">Titel:</div></td>
+                                      <td><font size="3"> 
+                                        <input name="topic" type="text" class="input" id="topic" size="120" value="Nutrition Profess | ">
+                                        </font></td>
+                                      
+                                    </tr>
+									<tr>
+										<td align="right">
+										<font size="3"> Picture:</font>
+                                                
+										</td>
+										<td>
+										<input name="newphoto" type="file" class="input" id="newphoto" size="40">
+										</td>
+									</tr>
+                                    <tr bgcolor="#FFFFFF"> 
+                                      <td rowspan="2" valign="top"> <div align="right"><font size="2">Detail:</font></div></td>
+                                      <td width="469" rowspan="2"><font size="3"> 
+<textarea name="message" cols="180" rows="30" class="input" id="message">
+<div align="left">
+<font color="#000000" face="tahoma" size="3">
+<a href="." target="_blank" ></a>
+
+
+
+
+</font>
+</div>
+</textarea>
+                                      </font>
+									  <br><br>
+									  Update on :<input name="update" type="text" class="input" id="update" size="12" value='<? echo $e_date; ?>' maxlength="100" readonly> *Cannot edit !
+
+									  
+									  
+									  </td>
+                                    </tr>
+									
+									
+                                    <tr bgcolor="#FFFFFF">
+                                      <td width="29" class="input">
+									  
+
+									  
+									  <p><br>
+                                      </p>                                      </td>
+                                      
+                                    </tr>
+                                    
+                                    <tr bgcolor="#FFFFFF"> 
+                                      <td valign="top">&nbsp;</td>
+                                      
+                                      <td><font size="3">
+                                        <br> 
+                                        <input name="submit" type="submit" class="submit" value="Submit" onClick="return confirm ('Sure ? ') ">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <a href="newmain.php" target="" >Cancel</a>
+                                        </font>
+                                      </td>
+                                      
+                                    </tr>
+                                  </form>
+                                </table>
+
+  
 </div>
 </body>
 </html>
--- php/scr/nu_news/NewsSave.php
+++ php/scr/nu_news/NewsSave.php
@@ -1,60 +1,85 @@
 <?php
-// Start session and output buffering
 @session_start();
+
 ob_start();
-
-// Check if user is logged in as administrator
 $useradmin = $_SESSION["useradmin"];
-if(empty($useradmin)) {
-    echo "<script>alert('Only Administrator');</script>";
-    header("Location: ../index.php");
-    exit();
+if(empty($useradmin)) 
+{
+echo "<script>alert('Only Administrator');</script>";
+header("Location: ../index.php");
+exit();
 }
-
-// Include necessary files
 require_once "../include/tdate.php";
 require_once "../include/connectdb.php";
 
-// Retrieve user information
-$sql = "SELECT * FROM useradmin WHERE useradmin=?";
-$stmt = mysqli_prepare($connect, $sql);
-mysqli_stmt_bind_param($stmt, "s", $useradmin);
-mysqli_stmt_execute($stmt);
-$result = mysqli_stmt_get_result($stmt);
-
-// Check if user information is found
-if($result && $result->num_rows > 0) {
-    $row = mysqli_fetch_assoc($result);
-    // Retrieve necessary user information if needed
-} else {
-    echo "<script>alert('Invalid User');</script>";
-    header("Location: ../index.php");
-    exit();
-}
-
-// Process file upload if applicable
+						  $sql="select * from useradmin where useradmin='$useradmin'";
+						  $db_query=mysqli_query($connect, $sql);					
+						  $result=mysqli_fetch_array($db_query);
+						  $id=$result["id"];
+						  $adminname=$result["name"];
+						  $user_admin=$result["useradmin"];
+						  $pass_admin=$result["passadmin"];
+						  
+if($_FILES["newphoto"]["name"] != "")
+{
+$name=$_FILES['newphoto']['name'];
+$tmp=$_FILES['newphoto']["tmp_name"];
+$date_time=date("Y-m-d H:i:s");
+$oldname=explode(".",$name);
+$ext = "";
+$ext = ".".$oldname[count($oldname)-1];
+$photo_1 =date('YmdHis').$ext;
+copy($tmp,"NEW/".$photo_1);
+}else{
 $photo_1 = "";
-if(isset($_FILES["newphoto"]["name"]) && $_FILES["newphoto"]["name"] != "") {
-    $name = $_FILES['newphoto']['name'];
-    $tmp = $_FILES['newphoto']['tmp_name'];
-    // Generate unique file name and move the file to the desired location
-    $photo_1 = "NEW/" . uniqid() . '_' . $name;
-    move_uploaded_file($tmp, $photo_1);
 }
+						  
+$postmessage = str_replace("\n","<br>",$_POST['message']);
 
-// Sanitize user input for message
-$postmessage = htmlspecialchars($_POST['message']);
 
-// Construct SQL INSERT statement
-$sql = "INSERT INTO news (topic, newphoto, message, dateregist) VALUES (?, ?, ?, ?)";
-$stmt = mysqli_prepare($connect, $sql);
-mysqli_stmt_bind_param($stmt, "ssss", $_POST['topic'], $photo_1, $postmessage, "$e_date $etime");
-$result = mysqli_stmt_execute($stmt);
 
-// Check if SQL INSERT was successful
-if(!$result) {
+$insert = "INSERT INTO news (topic, newphoto, message, dateregist) VALUES ('$_POST[topic]', '$photo_1', '$postmessage', '$e_date $etime')";
+$result = mysqli_query($connect, $insert);
+if (!$result) {
     die("Cannot Add Database: " . mysqli_error($connect));
 }
 ?>
+<html>
+<head>
+<title><? echo "$headtxt"; ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=tis-620">
+<meta http-equiv="refresh" content="900;url=../logout.php" />
 
-<!-- HTML content for displaying success message or redirecting users -->
+<!-- Fireworks MX Dreamweaver MX target.  Created Sat Apr 02 10:29:23 GMT+0700 (SE Asia Standard Time) 2011-->
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<meta http-equiv="refresh" content="3;URL=newmain.php">
+</head>
+<body bgcolor="#ffffff">
+<div align="center"> 
+
+            <table width="100%" border="0" cellspacing="1" cellpadding="1">
+                <tr> 
+                    <td>
+						<div align="center">
+						<br>
+                        <br>	
+		<img src="../images/ajax-loader.gif" width="" height="">			
+                        <br>
+                        <br>
+        <font color="#003366" size="2">Complated</font>
+						<br>
+						<br> 
+		<font size="2">Please Wait...</font>
+						<br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+						
+						</div>
+					</td>
+                </tr>
+            </table>
+</div>
+</body>
+</html>
