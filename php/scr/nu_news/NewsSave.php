9618336c9f1069be49fe5306fff9b9529ce04964
BLOOM <k-tanom@hotmail.com>
2024-05-24 11:33:29 +0700

    Update NewsSave.php

--- php/scr/nu_news/NewsSave.php
+++ php/scr/nu_news/NewsSave.php
@@ -5,60 +5,45 @@ ob_start();
 $useradmin = $_SESSION["useradmin"];
 if(empty($useradmin)) 
 {
-    echo "<script>alert('Only Administrator');</script>";
-    header("Location: ../index.php");
-    exit();
+echo "<script>alert('Only Administrator');</script>";
+header("Location: ../index.php");
+exit();
 }
 require_once "../include/tdate.php";
 require_once "../include/connectdb.php";
 
-// Retrieve user inputs and escape them
-$topic = mysqli_real_escape_string($connect, $_POST['topic']);
-$postmessage = mysqli_real_escape_string($connect, $_POST['message']);
-
+						  $sql="select * from useradmin where useradmin='$useradmin'";
+						  $db_query=mysqli_query($connect, $sql);					
+						  $result=mysqli_fetch_array($db_query);
+						  $id=$result["id"];
+						  $adminname=$result["name"];
+						  $user_admin=$result["useradmin"];
+						  $pass_admin=$result["passadmin"];
+						  
 if($_FILES["newphoto"]["name"] != "")
 {
-    // Handle file upload
-    $name = $_FILES['newphoto']['name'];
-    $tmp = $_FILES['newphoto']["tmp_name"];
-    $date_time = date("Y-m-d H:i:s");
-    $oldname = explode(".", $name);
-    $ext = "." . end($oldname);
-    $photo_1 = date('YmdHis') . $ext;
-    copy($tmp, "NEW/" . $photo_1);
+$name=$_FILES['newphoto']['name'];
+$tmp=$_FILES['newphoto']["tmp_name"];
+$date_time=date("Y-m-d H:i:s");
+$oldname=explode(".",$name);
+$ext = "";
+$ext = ".".$oldname[count($oldname)-1];
+$photo_1 =date('YmdHis').$ext;
+copy($tmp,"NEW/".$photo_1);
+}else{
+$photo_1 = "";
 }
-else
-{
-    $photo_1 = "";
-}
+						  
+$postmessage = str_replace("\n","<br>",$_POST['message']);
 
-// Prepare the SQL statement
-$insert = "INSERT INTO news (topic, newphoto, message, dateregist) VALUES (?, ?, ?, ?)";
-$stmt = mysqli_prepare($connect, $insert);
 
-if (!$stmt) {
-    die("Error in preparing statement: " . mysqli_error($connect));
-}
 
-// Bind parameters and execute the statement
-$dateregist = $e_date.$etime;
-mysqli_stmt_bind_param($stmt, "ssss", $topic, $photo_1, $postmessage, $dateregist);
-
-$result = mysqli_stmt_execute($stmt);
-
+$insert = "INSERT INTO news (topic, newphoto, message, dateregist) VALUES ('$_POST[topic]', '$photo_1', '$postmessage', '$e_date $etime')";
+$result = mysqli_query($connect, $insert);
 if (!$result) {
-    die("Error in executing statement: " . mysqli_stmt_error($stmt));
+    die("Cannot Add Database: " . mysqli_error($connect));
 }
-
-// Close the statement
-mysqli_stmt_close($stmt);
-
-// Redirect after insertion
-header("Location: newmain.php");
-exit();
 ?>
-
-
 <html>
 <head>
 <title><? echo "$headtxt"; ?></title>
