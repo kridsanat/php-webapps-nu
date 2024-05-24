28bd5927f61e07cabdffe0506bcb46a2c8865ed5
BLOOM <k-tanom@hotmail.com>
2024-05-24 11:13:37 +0700

    up

--- php/scr/nu_news/EditNewsSave.php
+++ php/scr/nu_news/EditNewsSave.php
@@ -1,42 +1,65 @@
 <?php
 @session_start();
-ob_start();
 
+ob_start();
 $useradmin = $_SESSION["useradmin"];
-if(empty($useradmin)) {
-    echo "<script>alert('Only Administrator');</script>";
-    header("Location: ../index.php");
-    exit();
+if(empty($useradmin)) 
+{
+echo "<script>alert('Only Administrator');</script>";
+header("Location: ../index.php");
+exit();
 }
 require_once "../include/tdate.php";
 require_once "../include/connectdb.php";
 
-$sql = "SELECT * FROM useradmin WHERE useradmin=?";
-$stmt = mysqli_prepare($connect, $sql);
-mysqli_stmt_bind_param($stmt, "s", $useradmin);
-mysqli_stmt_execute($stmt);
-$result = mysqli_stmt_get_result($stmt);
-
-if($result && $result->num_rows > 0) {
-    $row = mysqli_fetch_assoc($result);
-    $id = $row["id"];
-    $adminname = $row["name"];
-    $user_admin = $row["useradmin"];
-    $pass_admin = $row["passadmin"];
-} else {
-    echo "<script>alert('Invalid User');</script>";
-    header("Location: ../index.php");
-    exit();
-}
-
-$postmessage = str_replace("\n", "", $_POST['message']);
-
-$sql_data = "UPDATE news SET topic=?, message=?, dateregist=? WHERE id=?";
-$stmt = mysqli_prepare($connect, $sql_data);
-mysqli_stmt_bind_param($stmt, "sssi", $_POST['topic'], $postmessage, "$e_date $etime", $_POST['idx']);
-$result = mysqli_stmt_execute($stmt);
-
-if(!$result) {
-    die("Cannot Update Database: " . mysqli_error($connect)); // Display SQL error
-}
+						  $sql="select * from useradmin where useradmin='$useradmin'";
+						  $db_query=mysqli_query($connect, $sql);					
+						  $result=mysqli_fetch_array($db_query);
+						  $id=$result["id"];
+						  $adminname=$result["name"];
+						  $user_admin=$result["useradmin"];
+						  $pass_admin=$result["passadmin"];
+						  
+$postmessage = str_replace("\n","",$_POST['message']);
+$sql_data = "update news set topic='$_POST[topic]',message='$postmessage',dateregist='$e_date $etime' where id = '$_POST[idx]'";
+mysqli_query($connect,$sql_data);
 ?>
+<html>
+<head>
+<title><? echo "$headtxt"; ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=tis-620">
+<meta http-equiv="refresh" content="900;url=../logout.php" />
+
+<!-- Fireworks MX Dreamweaver MX target.  Created Sat Apr 02 10:29:23 GMT+0700 (SE Asia Standard Time) 2011-->
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<meta http-equiv="refresh" content="3;URL=newmain.php">
+</head>
+<body bgcolor="#ffffff">
+<div align="center"> 
+
+            <table width="100%" border="0" cellspacing="1" cellpadding="1">
+                <tr> 
+                    <td>
+						<div align="center">
+						<br>
+                        <br>	
+		<img src="../images/ajax-loader.gif" width="" height="">			
+                        <br>
+                        <br>
+        <font color="#003366" size="2">Complated</font>
+						<br>
+						<br> 
+		<font size="2">Please Wait...</font>
+						<br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+						
+						</div>
+					</td>
+                </tr>
+            </table>
+</div>
+</body>
+</html>
