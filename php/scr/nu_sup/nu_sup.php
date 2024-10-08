b76150bab576a76389acff298413556f64d99300
BLOOM <k-tanom@hotmail.com>
2024-10-08 12:01:44 +0700

    Update nu_sup.php

--- php/scr/nu_sup/nu_sup.php
+++ php/scr/nu_sup/nu_sup.php
@@ -324,7 +324,7 @@ if($bgmod==0){
 		 
 								<td width="25%" bgcolor='#AFEEEE' align="left">
 								&nbsp;&nbsp;	
-								<?php echo "<font face=tahoma size=4 color=#000000 >$info1 </font>"; ?>	
+								<?php echo "<font face=tahoma size=4 color=#3709e2 >$info1 </font>"; ?>	
 								&nbsp;						
 								<textarea name="info6" type="text" id="" id="info6" cols="47" rows="1" readonly><?php echo $info6; ?></textarea>
 								</td>	
