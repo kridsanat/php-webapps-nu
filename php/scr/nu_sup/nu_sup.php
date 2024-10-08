475e517807116cfcb2b5308ada92e40d9dbbd8b3
BLOOM <k-tanom@hotmail.com>
2024-10-08 09:55:43 +0700

    Update nu_sup.php

--- php/scr/nu_sup/nu_sup.php
+++ php/scr/nu_sup/nu_sup.php
@@ -297,7 +297,7 @@ if($bgmod==0){
 
 								<td bgcolor="#AFEEEE" width="50" valign="right"  >
 
-										<?php echo "../pic/$supphoto"; ?>
+										<a href="<?php echo "../pic/$supphoto"; ?>" >
 												
 										<?php
 							 			 if ($supphoto >= 100)
@@ -309,7 +309,7 @@ if($bgmod==0){
 									    }
 							 			 ?>
 		
-								
+									</a>
 												
 												
 								</td>	
