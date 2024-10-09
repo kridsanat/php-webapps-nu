4d66d969a4e9a36483177e9aec3b2f5d18a8e85b
BLOOM <k-tanom@hotmail.com>
2024-10-09 10:16:02 +0700

    Update nu_sup.php

--- php/scr/nu_sup/nu_sup.php
+++ php/scr/nu_sup/nu_sup.php
@@ -321,7 +321,7 @@ if($bgmod==0){
 															
 												
 												
-								<td bgcolor="#13e519" width="28%" valign="top" >
+								<td bgcolor="#22d027" width="28%" valign="top" >
 											
 									
 								&nbsp;<input name='submit' type='submit' class='submit' value='Edit' >&nbsp;
