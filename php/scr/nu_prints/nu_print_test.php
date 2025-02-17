d2f8b3cab44e3795783becefaea3cd9938a3b8ef
BLOOM <k-tanom@hotmail.com>
2025-02-17 10:25:09 +0700

    Update nu_print_test.php

--- php/scr/nu_prints/nu_print_test.php
+++ php/scr/nu_prints/nu_print_test.php
@@ -92,7 +92,7 @@ if (mysqli_num_rows($fect) == 0) {
         }
 
         echo "<tr style='background-color: $bgcolor;'>
-                <td>" . $rows["infono"] . "</td><td bgcolor='#4682B4' width='14%' ><font face= 'tahoma' color='#FFFACD' size='+1'><b>&nbsp;&nbsp;$infono</b></font></td>
+                <td bgcolor='#4682B4' width='14%' ><font face= 'tahoma' color='#FFFACD' size='+1'><b>&nbsp;&nbsp;" . $rows["infono"] . "</b></font></td>
                 <td>" . $info1 . "</td>
                 <td>" . $rows["info2"] . "</td>
                 <td>" . $printsprice . "</td>
