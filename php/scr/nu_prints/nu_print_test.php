b3a6f635165bcb9516dcb8fb61eefdfa87447630
BLOOM <k-tanom@hotmail.com>
2025-02-17 11:17:40 +0700

    Update nu_print_test.php

--- php/scr/nu_prints/nu_print_test.php
+++ php/scr/nu_prints/nu_print_test.php
@@ -106,8 +106,9 @@ $infono =$rows["infono"];
                 <td bgcolor='#4682B4' width='14%' ><font face= 'tahoma' color='#FFFACD' size='+1'><b>&nbsp;&nbsp;" . $rows["infono"] . "&nbsp;&nbsp;</b></font></td>
                 <td><font face= 'tahoma' color='#27408B' size='+1'><b>&nbsp;&nbsp;" . $rows["info2"] . "&nbsp;&nbsp;</b></font></td>
                 <td><font face= 'tahoma' color='#27408B' size='+1'>&nbsp;&nbsp;" . $info4 . "&nbsp;&nbsp;</font></td>
-                <br>
+                
               </tr>";
+              <br>
         $bgcount++;
     }
     echo "</table>";
