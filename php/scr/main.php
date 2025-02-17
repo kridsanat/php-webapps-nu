eb1570aa20a52ae7fe0cb43f6b9f9709804cacc1
BLOOM <k-tanom@hotmail.com>
2025-02-17 12:06:25 +0700

    Update main.php

--- php/scr/main.php
+++ php/scr/main.php
@@ -162,7 +162,7 @@ $adminname = htmlspecialchars($result["name"], ENT_QUOTES, 'UTF-8');
             <button onclick="location.href='../ChangePass.php'">Change Password</button>
             <button onclick="location.href='../logout.php'">Sign Out</button>
         </div>
-        <h1>Welcome : <font face= 'tahoma' color='#27408B' size='+1'><b><?php echo $adminname; ?>!</b></font></h1>
+        <h1>Welcome : <font face= 'tahoma' color='#27408B' size='+5'><b><?php echo $adminname; ?>!</b></font></h1>
     </header>
 
     <div class="container">
