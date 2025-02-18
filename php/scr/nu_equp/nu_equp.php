e180cb1622378405b5982907611ee077d77e882f
BLOOM <k-tanom@hotmail.com>
2025-02-18 11:29:06 +0700

    Update nu_equp.php

--- php/scr/nu_equp/nu_equp.php
+++ php/scr/nu_equp/nu_equp.php
@@ -226,8 +226,9 @@ $infono =$rows["infono"];
                             <font face= 'tahoma' color='#27408B' size='+1'><b>&nbsp;&nbsp;" . $rows["info2"] . "&nbsp;&nbsp;</font>
                             
                             <br>
-                            <font face= 'tahoma' color='#27408B' size='1'>&nbsp;&nbsp;&nbsp;&nbsp;" . $rows["info3"] . "&nbsp;&nbsp;</font>
-                            <input name='submit' type='submit' class='submit' value='Edit' >&nbsp;&nbsp;
+                            <input name='submit' type='submit' class='submit' value='Edit' >
+                            <font face= 'tahoma' color='#27408B' size='1'>&nbsp;&nbsp;" . $rows["info3"] . "&nbsp;&nbsp;</font>
+                            
                         </td>
                    
 
