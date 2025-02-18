9b841bc0d993d91623e87e840c0991f46b3887d6
BLOOM <k-tanom@hotmail.com>
2025-02-18 11:19:54 +0700

    Update nu_equp.php

--- php/scr/nu_equp/nu_equp.php
+++ php/scr/nu_equp/nu_equp.php
@@ -190,7 +190,7 @@ if (mysqli_num_rows($fect) == 0) {
                 <th><font face= 'tahoma' color='#27408B' size='1'>สถาณที่</font></th>
                 <th width='200' ><font face= 'tahoma' color='#27408B' size='1'>ผู้แก้ไข</font></th>
                 <th><font face= 'tahoma' color='#27408B' size='1'>รายละเอียด</font></th>
-                <th width='50'><font face= 'tahoma' color='#27408B' size='1'>จำนวน</font></th>
+                <th width='100'><font face= 'tahoma' color='#27408B' size='1'>จำนวน</font></th>
                 <th width='100'><font face= 'tahoma' color='#27408B' size='1'>ราคา</font></th>
                 <th width='100'><font face= 'tahoma' color='#27408B' size='1'>รวม</font></th>
             </tr>";
