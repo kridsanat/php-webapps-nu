ad322d67d0f80d72828aa261a4028c3feb89186d
BLOOM <k-tanom@hotmail.com>
2025-05-29 10:28:45 +0700

    Update ShowNews.php

--- php/scr/nu_news/ShowNews.php
+++ php/scr/nu_news/ShowNews.php
@@ -56,7 +56,7 @@ background-attachment : fixed
 		<head>
 		<?php echo "$fav"; ?>
 			<title><?php echo "$topic"; ?></title>
-			<meta http-equiv="Content-Type" content="text/html; charset=tis-620">
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 			<meta http-equiv="refresh" content="1800;url=../logout.php" />
 
 			<link href="" rel="" type="">
@@ -143,9 +143,8 @@ background-attachment : fixed
                               <td>
 							  <font size="2" face="tahoma" color="#222222">
 
-						
-							  <?php echo nl2br(htmlspecialchars_decode($message, ENT_QUOTES)); ?>
-
+							  <? echo "$message"; ?>
+							  
 							  
 							
 							  <?php 
