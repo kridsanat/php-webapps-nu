98c3c639c424f211c838d9a83d9e38a309d1263b
BLOOM <k-tanom@hotmail.com>
2025-05-29 10:39:06 +0700

    x

--- php/scr/nu_news/EditNewsPhotoSave.php
+++ php/scr/nu_news/EditNewsPhotoSave.php
@@ -1,82 +1,67 @@
 <?php
 @session_start();
-
 ob_start();
+
 $useradmin = $_SESSION["useradmin"];
-if(empty($useradmin)) 
-{
-echo "<script>alert('Only Administrator');</script>";
-header("Location: ../index.php");
-exit();
+if (empty($useradmin)) {
+    echo "<script>alert('Only Administrator');</script>";
+    header("Location: ../index.php");
+    exit();
 }
+
 require_once "../include/tdate.php";
 require_once "../include/connectdb.php";
 
-						  $sql="select * from useradmin where useradmin='$useradmin'";
-						  $db_query=mysqli_query($connect, $sql);					
-						  $result=mysqli_fetch_array($db_query);
-						  $id=$result["id"];
-						  $adminname=$result["name"];
-						  $user_admin=$result["useradmin"];
-						  $pass_admin=$result["passadmin"];
-						  
-						  
-if($_FILES["newphoto"]["name"] != "")
-{
-$name=$_FILES['newphoto']['name'];
-$tmp=$_FILES['newphoto']["tmp_name"];
-$date_time=date("Y-m-d H:i:s");
-$oldname=explode(".",$name);
-$ext = "";
-$ext = ".".$oldname[count($oldname)-1];
-$photo_1 =date('YmdHis').$ext;
-copy($tmp,"NEW/".$photo_1);
-}else{
+// ตรวจสอบผู้ใช้งาน
+$sql = "SELECT * FROM useradmin WHERE useradmin='$useradmin'";
+$db_query = mysqli_query($connect, $sql);					
+$result = mysqli_fetch_array($db_query);
+$id = $result["id"];
+$adminname = $result["name"];
+
+// อัปโหลดรูป
 $photo_1 = "";
-}				  
-						  
-						  
-$postmessage = str_replace("\n","",$_POST['message']);
-$sql_data = "update news set topic='$_POST[topic]',newphoto='$photo_1',message='$postmessage',dateregist='$e_date $etime' where id = '$_POST[idx]'";
-mysqli_query($connect,$sql_data);
+if ($_FILES["newphoto"]["name"] != "") {
+    $name = $_FILES['newphoto']['name'];
+    $tmp = $_FILES['newphoto']["tmp_name"];
+    $ext = "." . pathinfo($name, PATHINFO_EXTENSION);
+    $photo_1 = date('YmdHis') . $ext;
+    copy($tmp, "NEW/" . $photo_1);
+}
+
+// อัปเดตเฉพาะรูปถ้ามีรูปใหม่
+if (!empty($photo_1)) {
+    $idx = intval($_POST['idx']);
+    $sql_data = "UPDATE news SET newphoto='$photo_1', dateregist='$e_date $etime' WHERE id='$idx'";
+    mysqli_query($connect, $sql_data);
+}
 ?>
 <html>
 <head>
 <?php echo "$fav"; ?>
-<title><? echo "$headtxt"; ?></title>
-<meta http-equiv="Content-Type" content="text/html; charset=tis-620">
+<title><?php echo "$headtxt"; ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="refresh" content="900;url=../logout.php" />
-
-<!-- Fireworks MX Dreamweaver MX target.  Created Sat Apr 02 10:29:23 GMT+0700 (SE Asia Standard Time) 2011-->
-<link href="css/style.css" rel="stylesheet" type="text/css">
 <meta http-equiv="refresh" content="3;URL=newmain.php">
+<link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body bgcolor="#ffffff">
-<div align="center"> 
-
-            <table width="100%" border="0" cellspacing="1" cellpadding="1">
-                <tr> 
-                    <td>
-						<div align="center">
-						<br>
-                        <br>	
-		<img src="../images/ajax-loader.gif" width="" height="">			
-                        <br>
-                        <br>
-        <font color="#003366" size="2">Complated</font>
-						<br>
-						<br> 
-		<font size="2">Please Wait...</font>
-						<br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-						
-						</div>
-					</td>
-                </tr>
-            </table>
+<div align="center">
+    <table width="100%" border="0" cellspacing="1" cellpadding="1">
+        <tr> 
+            <td>
+                <div align="center">
+                    <br><br>
+                    <img src="../images/ajax-loader.gif">
+                    <br><br>
+                    <font color="#003366" size="2">Completed</font>
+                    <br><br>
+                    <font size="2">Please Wait...</font>
+                    <br><br><br><br><br>
+                </div>
+            </td>
+        </tr>
+    </table>
 </div>
 </body>
 </html>
